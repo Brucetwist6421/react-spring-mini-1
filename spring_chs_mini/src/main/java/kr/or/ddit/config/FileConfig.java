5ac40e60bef559963351c0c29218b75dd81f3ccc
HanSung <chs6421@gmail.com>
2026-01-23 12:35:45 +0900

    Fix: Change react Dockerfile_4

--- spring_chs_mini/src/main/java/kr/or/ddit/config/FileConfig.java
+++ spring_chs_mini/src/main/java/kr/or/ddit/config/FileConfig.java
@@ -16,9 +16,11 @@ public class FileConfig implements WebMvcConfigurer {
 	 * D:/springboot/upload/2025/02/20/개똥이.jpg
 	 */
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		log.debug("=====addResourceHandlers 실행======");
-		registry.addResourceHandler("/upload/**")
-				.addResourceLocations("file:///C:/upload/");
-	}
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 1. 브라우저에서 /upload/** 로 들어오는 모든 요청을 가로챕니다.
+        registry.addResourceHandler("/upload/**")
+                // 2. 실제 리눅스 서버의 물리적 경로와 연결합니다.
+                // file:/ 형식으로 시작해야 하며 끝에 반드시 /를 붙여야 합니다.
+                .addResourceLocations("file:/home/ubuntu/upload/");
+    }
 }
