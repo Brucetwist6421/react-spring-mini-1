6d0a40636d6df2fa3aba26215517fee9489cbf47
HanSung <chs6421@gmail.com>
2026-02-10 14:54:05 +0900

    start security6

--- spring_chs_mini/src/main/java/kr/or/ddit/service/impl/AuthServiceImpl.java
+++ spring_chs_mini/src/main/java/kr/or/ddit/service/impl/AuthServiceImpl.java
@@ -7,11 +7,14 @@ import kr.or.ddit.vo.LoginResponseVO;
 import kr.or.ddit.vo.MemberVO;
 import kr.or.ddit.config.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthServiceImpl implements AuthService {
 
     private final MemberMapper memberMapper;
@@ -22,6 +25,12 @@ public class AuthServiceImpl implements AuthService {
     public LoginResponseVO authenticate(LoginRequestVO loginVO) {
         // 1. 유저 조회
         MemberVO user = memberMapper.findByEmail(loginVO.getEmail());
+
+        if (user != null) {
+            log.info("DB에서 가져온 암호화된 비번: [{}]", user.getPassword());
+        } else {
+            log.error("DB에 해당 이메일을 가진 유저가 없습니다.");
+        }
 
         if (user == null) {
             throw new RuntimeException("존재하지 않는 사용자입니다.");
