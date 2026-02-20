45b03a3dcf0ffaa3c4311f67e6a3573e43a9e70f
HanSung <chs6421@gmail.com>
2026-02-20 12:24:30 +0900

    account detail backLayer

--- spring_chs_mini/src/main/java/kr/or/ddit/mapper/AccountMapper.java
+++ spring_chs_mini/src/main/java/kr/or/ddit/mapper/AccountMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.checkerframework.checker.units.qual.A;
 
 import kr.or.ddit.vo.AccountVO;
 
