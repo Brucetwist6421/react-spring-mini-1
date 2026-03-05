3c513f477b3248ed1ae3574e5592604e7eb7c337
HanSung <chs6421@gmail.com>
2026-03-05 09:59:50 +0900

    student data localStorage data add

--- spring_chs_mini/src/main/java/kr/or/ddit/vo/LoginResponseVO.java
+++ spring_chs_mini/src/main/java/kr/or/ddit/vo/LoginResponseVO.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.vo;
 
+
+import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data // Getter, Setter, ToString, EqualsAndHashCode 포함
 @NoArgsConstructor // 마이바티스용 기본 생성자
 @AllArgsConstructor // 전체 필드 생성자
+@Builder // 빌더 패턴 지원
 public class LoginResponseVO {
     String accessToken;
     String accId;
